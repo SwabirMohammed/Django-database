bac8e961b0309cbccf29eb7d948ac7da7490d034
Swabir <swabir49@gmail.com>
2023-02-28 10:18:03 +0300

    authentication forms

--- Swabir_DjangoDatabases/urls.py
+++ Swabir_DjangoDatabases/urls.py
@@ -15,14 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.urls import path
 from . import views
 
 app_name = "main"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", views.homepage, name="homepage"),
-    path("register", views.register_request, name="register")
+    path('register', views.register_request, name="register")
 
 ]
